b4da89fba058c8c5d13ff3442f1980f75a2bc239
kasa-umbrella <nk1101ruvt@gmail.com>
2026-03-20 22:03:07 +0900

    ホームビューコンポーネントとそのサブコンポーネント（ダッシュボード、リンク、記事一覧）を追加し、ログアウト機能を実装しました。

---
+++ next/features/manage/home/components/HomeLinks.tsx
@@ -0,0 +1,13 @@
+import AppHeadTitle from "@/util/components/AppHeadTitle";
+import { Box } from "@mui/material";
+
+const HomeLinks = () => {
+    return (
+        <Box>
+            <AppHeadTitle>メニュー</AppHeadTitle>
+            {/* TODO: リンクボタン */}
+        </Box>
+    );
+};
+
+export default HomeLinks;
